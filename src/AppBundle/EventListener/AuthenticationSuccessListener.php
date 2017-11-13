e07c6bf67da89b4c8716a226a20cb5c3fd48ca5d
Kamil Woźny <kamil.wozny@polcode.net>
2017-11-13 13:07:09 +0100

    added roles to token response

---
+++ src/AppBundle/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use AppBundle\Entity\User\UserInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+
+class AuthenticationSuccessListener
+{
+    /**
+     * @param AuthenticationSuccessEvent $event
+     */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $data['roles'] = $user->getRoles();
+
+        $event->setData($data);
+    }
+}
